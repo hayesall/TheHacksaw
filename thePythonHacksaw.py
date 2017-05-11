1adc6b48716daab8e2263f378a070f6f5435cc3e
TheVanishingMan <alexander@batflyer.net>
2017-05-10 21:40:48 -0400

    Cleaning up the hacksaw tree. This project got fairly entangled with the ER-diagram walking project, so I'm clearing out the files that are not relevant to the hacksaw/autograder.

--- thePythonHacksaw.py
+++ thePythonHacksaw.py
@@ -6,16 +6,20 @@ BoostSRL-Hacksaw v0.02
    Last updated: May 9, 2017
 
 Sample Calls:
-   $ python thePythonHacksaw.py v1-0.jar
+   $ python thePythonHacksaw.py
 '''
 
 from __future__ import print_function
 from scipy import stats
+from sklearn import cross_validation
+
 #from tabulate import tabulate
-import numpy as np
 import os
 import re
 import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 # https://github.com/google/python-subprocess32
 if os.name == 'posix' and sys.version_info[0] < 3:
@@ -23,11 +27,10 @@ if os.name == 'posix' and sys.version_info[0] < 3:
 else:
     import subprocess
 
-EPOCHS = 25
-TREES = 10
+EPOCHS = 2
+TREES = 5
+RDNJARPATH = ' v1-0.jar '
 AUCJARPATH = ' -aucJarPath .'
-
-# DATASETS: a list of tuples: 'Name-of-directory', 'target'
 
 DATASETS = [['Cora', 'sameauthor', 9],
             ['WebKB', 'faculty', 5]]
@@ -36,80 +39,194 @@ ALGOS = [['RDN-Boost', '']]
 
 FLAGS = ['tushar', '-e', '-rw', '-w', '-s']
 
-for d in DATASETS:
-    
-    dataset = d[0]
-    target = d[1]
-    number_of_features = d[2]
+def main():
 
-    for a in ALGOS:
+    for d in DATASETS:
 
-        params = a[1]
+        dataset = d[0]
+        target = d[1]
+        number_of_features = d[2]
 
-        for f in FLAGS:
-            
-            if f in ['-rw', '-w', '-s']:
-                for n in range(number_of_features):
+        for a in ALGOS:
 
-                    traintime = []
-                    roc = []
-                    pr = []
+            params = a[1]
 
-                    print(dataset, '| # of features:', n + 1, '| flag:', f)
+            for f in FLAGS:
+                print(dataset, '| flag:', f)
+
+                training_time_means, training_time_stds = [], []
+                roc_means, roc_stds = [], []
+                pr_means, pr_stds = [], []
+                
+                if f in ['-rw', '-w', '-s']:
+                    # Random Walk, Walk, or Shortest Walk
+
+                    for n in range(number_of_features):
+                        traintime, roc, pr = [], [], []
+                        
+                        print(dataset, '| # of features:', n + 1, '| flag:', f)
+                        
+                        for e in range(EPOCHS):
+                            
+                            # Create the modes file for this Train/Test epoch
+                            construct_modes(dataset, f, NUMBER=n)
+
+                            # BoostSRL Training
+                            train_model(dataset, params, target)
+
+                            # Find the time (in seconds) from the log file
+                            traintime.append(get_training_time())
+
+                            # BoostSRL Testing
+                            test_model(dataset, params, target)
+
+                            # Find the AUC ROC and AUC PR
+                            roc_score, pr_score = get_roc_and_pr_score()
+                            roc.append(roc_score)
+                            pr.append(pr_score)
+                        
+                        # Calculate mean and standard deviation for Training Time, AUC ROC, and AUC PR
+                        training_mean, training_std = np.mean(traintime), np.std(traintime)
+                        auc_roc_mean, auc_roc_std = np.mean(roc), np.std(roc)
+                        auc_pr_mean, auc_pr_std = np.mean(pr), np.std(pr)
+
+                        # Update the arrays holding the values, these will be used for plotting the information
+                        training_time_means.append(training_mean)
+                        training_time_stds.append(training_std)
+                        roc_means.append(auc_roc_mean)
+                        roc_stds.append(auc_roc_std)
+                        pr_means.append(auc_pr_mean)
+                        pr_stds.append(auc_pr_std)
+                        
+                        '''
+                        print_information(training_mean, training_std,
+                                          auc_roc_mean, auc_roc_std,
+                                          auc_pr_mean, auc_pr_std)
+                        '''
+
+                    name_to_save = dataset + '-' + f + '-' + str(EPOCHS) + '.png'
+
+                    plot_errorbars(training_time_means, training_time_stds,
+                                   roc_means, roc_stds,
+                                   pr_means, pr_stds, name_to_save)
+                    log_progress(training_time_means, training_time_stds,
+                                 roc_means, roc_stds,
+                                 pr_means, pr_stds, name_to_save)
+                    exit()
+                    
+                else:
+                    # for now, do not worry about these while I work on the code for them
+                    continue
+                    
                     for e in range(EPOCHS):
+                        print(dataset, '| flag:', f, '| epoch:', e)
                         
-                        #print(dataset, '| # of features:', n + 1, '| flag:', f, '| epoch:', e)
+def import_data(file_to_read):
+    if os.path.isfile(file_to_read):
+        with open(file_to_read, 'r') as f:
+            data = f.read().splitlines()
+        return data
+    else:
+        raise('Error, there were problems when reading ' + file_to_read)
 
-                        # Extra parameters to Cora Modes (won't work without them):
-                        if dataset == 'Cora':
-                            CALL = 'echo -e "setParam: maxTreeDepth=3.\nsetParam: nodeSize=2." > datasets/Cora/cora_bk.txt'
-                            p = subprocess.Popen(CALL, shell=True)
-                            os.waitpid(p.pid, 0)
+def data_validation(data):
+    #x_train, x_test, y_train, y_test = cross_validation()
+    pass
 
-                        # Create the modes file
-                        CALL = 'python walker2.py --number ' + str(n+1) + ' ' + f + ' ' + dataset + '.mayukh | grep "mode:" >> datasets/' + dataset + '/' + dataset.lower() + '_bk.txt'
-                        p = subprocess.Popen(CALL, shell=True)
-                        os.waitpid(p.pid, 0)
+def call_process(call):
+    p = subprocess.Popen(call, shell=True)
+    os.waitpid(p.pid, 0)
 
-                        # Call RDN-Boost Training
-                        #print('Training')
+def construct_modes(dataset, flag, NUMBER=None):
+    # Extra parameters to Cora Modes (won't work without them):
+    if dataset == 'Cora':
+        call_process('echo -e "setParam: maxTreeDepth=3.\nsetParam: nodeSize=2." > datasets/Cora/cora_bk.txt')
 
-                        CALL = 'java -jar v1-0.jar -l -train datasets/' + dataset + '/train/ ' + params + '-target ' + target + ' -trees ' + str(TREES) + ' > trainlog.txt'
-                        p = subprocess.Popen(CALL, shell=True)
-                        os.waitpid(p.pid, 0)
+    # Create the modes file
+    if NUMBER is not None:
+        CALL = 'python walker2.py --number ' + str(NUMBER) + ' ' + flag + ' ' + dataset + \
+               '.mayukh | grep "mode:" >> datasets/' + dataset + '/' + dataset.lower() + '_bk.txt'
+        call_process(CALL)
 
-                        # Find the time from the log file
-                        text = open('trainlog.txt','r').read()
-                        line = re.findall(r'trees\): \d*.\d*', text)
-                        traintime.append(float(line[0].split()[1]))
+def train_model(dataset, params, target):
+    # BoostSRL Training
+    CALL = 'java -jar' + RDNJARPATH + '-l -train datasets/' + dataset + '/train/ ' + params + '-target ' + \
+           target + ' -trees ' + str(TREES) + ' > trainlog.txt'
+    call_process(CALL)
 
-                        # Call RDN-Boost Testing
-                        #print('Testing')
-                        
-                        CALL = 'java -jar v1-0.jar -i -model datasets/' + dataset + '/train/models/ -test datasets/' + dataset + '/test/ -target ' + target + AUCJARPATH + ' -trees ' + str(TREES) + ' > testlog.txt'
-                        #subprocess.call(CALL.split())
-                        p = subprocess.Popen(CALL, shell=True)
-                        os.waitpid(p.pid, 0)
-                        
-                        text = open('testlog.txt','r').read()
-                        line = re.findall(r'AUC ROC   = \d.\d*|AUC PR    = \d.\d*', text)
-                        # ROC
-                        roc.append(float(line[0].split()[3]))
-                        # PR
-                        pr.append(float(line[1].split()[3]))
-                    print('Training Time |', np.mean(traintime), '+-', np.std(traintime))
-                    print('AUC ROC       |', np.mean(roc), '+-', np.std(roc))
-                    print('AUC PR        |', np.mean(pr), '+-', np.std(pr))
-                    #exit()
-                exit()
-            else:
-                for e in range(EPOCHS):
-                    print(dataset, '| flag:', f, '| epoch:', e)
+def test_model(dataset, params, target):
+    # BoostSRL Testing
+    CALL = 'java -jar' + RDNJARPATH + '-i -model datasets/' + dataset + '/train/models/ -test datasets/' + \
+           dataset + '/test/ -target ' + target + AUCJARPATH + ' -trees ' + str(TREES) + ' > testlog.txt'
+    call_process(CALL)
 
+def get_training_time():
+    text = open('trainlog.txt', 'r').read()
+    line = re.findall(r'trees\): \d*.\d* seconds', text)
+    if not line:
+        # Seconds should always be a decimal value, otherwise we need to deal in minutes and seconds
+        line = re.findall(r'trees\): \d* minutes and \d*.\d* seconds', text)
+        # Convert the minutes into seconds and add the seconds:
+        splitline = line[0].split()
+        seconds = float(splitline[1] * 60 + float(splitline[4]))
+    else:
+        seconds = float(line[0].split()[1])
+    return seconds
 
+def get_roc_and_pr_score():
+    text = open('testlog.txt','r').read()
+    line = re.findall(r'AUC ROC   = \d.\d*|AUC PR    = \d.\d*', text)
+    roc_score = float(line[0].split()[3])
+    pr_score = float(line[1].split()[3])
+    return roc_score, pr_score
 
+def print_information(training_mean, training_std, auc_roc_mean, auc_roc_std, auc_pr_mean, auc_pr_std):
+    print('Training Time |', training_mean, '+-', training_std)
+    print('AUC ROC       |', auc_roc_mean, '+-', auc_roc_std)
+    print('AUC PR        |', auc_pr_mean, '+-', auc_pr_std)
 
+def log_progress(training_time_means, training_time_stds, roc_means, roc_stds, pr_means, pr_stds, name_to_save):
+    print('Saving information for', name_to_save, 'to file.')
+    with open('hacksaw_log.txt', 'a') as f:
+        f.write(name_to_save + '\n' + \
+                str(training_time_means) + '\n' + str(training_time_stds) + '\n' + \
+                str(roc_means) + '\n' + str(roc_stds) + '\n' + \
+                str(pr_means) + '\n' + str(pr_stds) + '\n')
 
+def plot_errorbars(training_time_means, training_time_stds, roc_means, roc_stds, pr_means, pr_stds, name_to_save):
+    print('Saving image for', name_to_save)
+    
+    x_axis = range(len(training_time_means)+1)[1:]
+    fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(15,5))
+
+    # Name at the top
+    fig.suptitle(name_to_save)
+    
+    # ax0: Time
+    ax0.errorbar(x_axis, training_time_means, yerr=training_time_stds, fmt='-o')
+    ax0.set_xlabel('Number of Steps')
+    ax0.set_ylabel('Time')
+    ax0.set_title('Training Time')
+    ax0.set_xlim([0, len(training_time_means)+1])
+    ax0.set_ylim([0, max(training_time_means) + max(training_time_stds) + 1])
+    
+    # ax1: AUC ROC
+    ax1.errorbar(x_axis, roc_means, yerr=roc_stds, fmt='-o')
+    ax1.set_xlabel('Number of Steps')
+    ax1.set_ylabel('AUC ROC')
+    ax1.set_title('AUC ROC')
+    ax1.set_xlim([0, len(roc_means)+1])
+    ax1.set_ylim([0.4,1])
+    
+    # ax2: AUC PR
+    ax2.errorbar(x_axis, pr_means, yerr=pr_stds, fmt='-o')
+    ax2.set_xlabel('Number of Steps')
+    ax2.set_ylabel('AUC PR')
+    ax2.set_title('AUC PR')
+    ax2.set_xlim([0, len(pr_means)+1])
+    ax2.set_ylim([0.4,1])
+    
+    plt.savefig(name_to_save, dpi=600)
 
 '''
 DATASETS = [['Father', 'father'],
@@ -126,3 +243,4 @@ ALGOS = [['RDN-Boost', ''],
          ['LSTree Boosting Regression', '-reg ']]
 '''
 
+if __name__ == '__main__': main()
